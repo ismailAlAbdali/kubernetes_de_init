ebce37c3d9bb8f043f9630c0013c8c34a9d4496b
ismailAlAbdali <121197140+ismailAlAbdali@users.noreply.github.com>
2024-06-24 08:21:12 +0400

    fix syntax error

--- dags/fetch_and_preview.py
+++ dags/fetch_and_preview.py
@@ -8,13 +8,12 @@ import json
 
 def get_data(**kwargs):
     url = 'https://raw.githubusercontent.com/mrdbourke/zero-to-mastery-ml/master/data/car-sales-extended.csv'
-     # Make,Colour,Odometer (KM),Doors,Price
     response = requests.get(url)
 
     if response.status_code == 200:
-        df = pd.read_csv(url, header=None, names = ['Make','Color','Odometer (KM)','Doors','Price'])
+        df = pd.read_csv(url, header=0)  # Adjusted header to read column names from the CSV
 
-        # convert dataframe to json from xcom
+        # Convert dataframe to json from xcom
         json_data = df.to_json(orient='records')
 
         kwargs['ti'].xcom_push(key='data', value=json_data)
@@ -24,22 +23,22 @@ def get_data(**kwargs):
 
 def preview_data(**kwargs):
     output_data = kwargs['ti'].xcom_pull(key='data', task_ids='get_data')
-    print(output_data)
-    if output_data:
-        output_data = json.loads(output_data) # load the dict data to be json data
-    else:
-        raise ValueError(f'No Data recevied from XCom')
 
-    # Create Dataframe from the JSON data
+    if output_data:
+        output_data = json.loads(output_data)  # Load the dict data to be JSON data
+    else:
+        raise ValueError(f'No Data received from XCom')
+
+    # Create DataFrame from the JSON data
     df = pd.DataFrame(output_data)
 
-    # Compute and Aggreate based on Make
-    df_uni_make_color = df.groupby(['Make','Color'],as_index=False,dropna=True).aggregate({'Odometer (KM)':'avg', 'Price':'avg'})
+    # Compute and aggregate based on Make and Color
+    df_uni_make_color = df.groupby(['Make', 'Color'], as_index=False, dropna=True).agg(
+        {'Odometer (KM)': 'mean', 'Price': 'mean'})
 
     df_uni_make_color = df_uni_make_color.sort_values('Price', ascending=False)
 
-    print(df_uni_make_color[['Odometer (KM)','Price']].head(20))
-
+    print(df_uni_make_color[['Odometer (KM)', 'Price']].head(20))
 
 
 default_args = {
@@ -48,10 +47,20 @@ default_args = {
     'catchup': False
 }
 
-dag = DAG('fetch_and_review_car_data', default_args=default_args, schedule_interval = timedelta(days=1))
+dag = DAG('fetch_and_review_car_data', default_args=default_args, schedule_interval=timedelta(days=1))
 
-data_data_py_operator = PythonOperator(task_id='get_data', python_callable=get_data, dag=dag)
-preview_data_from_url = PythonOperator(task_id='preview_data', python_callable=preview_data, dag = dag)
+data_data_py_operator = PythonOperator(
+    task_id='get_data',
+    python_callable=get_data,
+    provide_context=True,
+    dag=dag
+)
 
+preview_data_from_url = PythonOperator(
+    task_id='preview_data',
+    python_callable=preview_data,
+    provide_context=True,
+    dag=dag
+)
 
 data_data_py_operator >> preview_data_from_url
